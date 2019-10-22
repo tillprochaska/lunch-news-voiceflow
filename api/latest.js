d1f8bf0b919debf697ac0aa4d4d902f4da6eb70c
Till Prochaska <mail@tillprochaska.de>
2019-10-22 19:38:51 +0200

    Fix response content type

--- api/latest.js
+++ api/latest.js
@@ -2,7 +2,6 @@ const fetchLatestArticle = require('../lib/fetchLatestArticle.js');
 
 module.exports = (req, res) => {
     fetchLatestArticle().then(article => {
-        const json = JSON.stringify({ article });
-        res.status(200).send(json);
+        res.status(200).json({ article });
     })
 };

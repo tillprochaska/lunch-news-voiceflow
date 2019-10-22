2c00596f0ae33d3319d226c9fc25da5be4e275c4
Till Prochaska <mail@tillprochaska.de>
2019-10-22 21:02:06 +0200

    Cache scraped article

--- api/latest.js
+++ api/latest.js
@@ -2,6 +2,7 @@ const fetchLatestArticle = require('../lib/fetchLatestArticle.js');
 
 module.exports = (req, res) => {
     fetchLatestArticle().then(article => {
+        res.setHeader('Cache-Control', 's-maxage=3600');
         res.status(200).json({ article });
     })
 };

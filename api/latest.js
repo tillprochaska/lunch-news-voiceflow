1cf66290c7e4f01ca32fcce4e8a0c29e36d59440
Till Prochaska <mail@tillprochaska.de>
2019-10-24 18:04:48 +0200

    Output RSS

--- api/latest.js
+++ api/latest.js
@@ -1,8 +1,33 @@
 const fetchLatestArticle = require('../lib/fetchLatestArticle.js');
+const md5 = require('md5');
 
 module.exports = (req, res) => {
     fetchLatestArticle().then(article => {
+        const date = new Date();
+        const items = article.map(item => {
+            return `
+                <item>
+                    <title>Hallo</title>
+                    <link>https://business-punk.com</link>
+                    <pubDate>${ date.toISOString() }</pubDate>
+                    <description>${ item }</description>
+                    <guid>${ md5(item) }</guid>
+                </item>
+            `;
+
+            const rss = `
+                <?xml version="1.0" encoding="UTF-8" ?>
+                <rss version="2.0">
+                    <channel>
+                        <ttl>30</ttl>
+                        ${ items.join('') }
+                    </channel>
+                </rss>
+            `;
+        });
         res.setHeader('Cache-Control', 's-maxage=3600');
-        res.status(200).json({ article });
-    })
+        res.setHeader('Content-Type', 'application/rss+xml');
+        res.status(200);
+        res.send(rss);
+    });
 };
